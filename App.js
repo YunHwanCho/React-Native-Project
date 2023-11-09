660a0c4c140f2f198d29ea08f4f795137f5c67cd
조윤환 <jyh6314@naver.com>
2023-11-09 13:07:47 +0900

    날씨 앱 만들기

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
+
+const screen_width = Dimensions.get("window").width;
 
 export default function App() {
+  const[city, setCity] = useState("Loading..")
+  const [location, setLocation] = useState();
+  const [ok, setOK] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOK(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city)
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        indicatorStyle="white"
+        npx
+        expo
+        install
+        expo-location
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +65,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "skyblue",
+  },
+  city: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cityName: {
+    fontWeight: 500,
+    fontSize: 58,
+  },
+  weather: {},
+  day: {
+    width: screen_width,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 170,
+    fontWeight: 500,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
