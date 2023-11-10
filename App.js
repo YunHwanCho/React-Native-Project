4f9c63c841b7a083376a52ff8312a3376f88104a
조윤환 <jyh6314@naver.com>
2023-11-10 13:41:55 +0900

    날씨앱 기능 구현 완료

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import * as Location from "expo-location";
-
+import { Ionicons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
 const screen_width = Dimensions.get("window").width;
+const API_KEY = "7bdd7dad75d39879712753e4effb33ef";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear : "day-sunny",
+  Atmosphere :"",
+  snow : "snowflake",
+  Rain : "rain",
+  Thunderstorm : "lightning"
+
+};
 
 export default function App() {
-  const[city, setCity] = useState("Loading..")
-  const [location, setLocation] = useState();
+  const [city, setCity] = useState("Loading..");
+  const [days, setDays] = useState([]);
   const [ok, setOK] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOK(false);
@@ -21,11 +39,30 @@ export default function App() {
       { latitude, longitude },
       { useGoogleMaps: false }
     );
-    setCity(location[0].city)
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+
+    const json = await response.json();
+    
+
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("03:00:00")) {
+          return weather;
+        }
+      })
+    );
+   
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
+  const extractDate = (dt_txt) => {
+    const date = new Date(dt_txt);
+    return `${date.getMonth() + 1}.${date.getDate()-1}`;
+  };
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -41,22 +78,33 @@ export default function App() {
         expo-location
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator style ={{alignItems : 'center', marginTop : 10}}
+              color="white"
+              size="large"
+              
+            />
+          </View>
+        ) : ( 
+          days.map((day, index) => (
+             
+            <View key={index} style={styles.day}>
+              <Text style={{...styles.day, fontSize : 50, fontWeight : 'bold',marginLeft :140, color : 'white'}}>{extractDate(day.dt_txt)}</Text>
+              <View style = {{flexDirection : 'row', alignItems : 'center', width :"100%", justifyContent : "space-between"}}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                
+                <Fontisto name={icons[day.weather[0].main]} size={60} color="black" margin style ={{marginTop : 80, marginRight : 50}}  />
+
+              </View>
+              
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -69,6 +117,7 @@ const styles = StyleSheet.create({
   },
   city: {
     flex: 1,
+    marginTop: 20,
 
     alignItems: "center",
     justifyContent: "center",
@@ -77,18 +126,27 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     fontSize: 58,
   },
-  weather: {},
   day: {
     width: screen_width,
-    alignItems: "center",
+
+    justifyContent : "flex-start",
+    
   },
   temp: {
     marginTop: 50,
-    fontSize: 170,
+    fontSize: 120,
     fontWeight: 500,
+    marginLeft : 50
   },
   description: {
+
     marginTop: -30,
-    fontSize: 60,
+    fontSize: 40,
+    marginLeft : 51
+  },
+  tinyText: {
+    
+    fontSize: 15,
+    marginLeft : 55
   },
 });
